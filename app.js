fa02f7ddfc272cb5db92391e48b77f04e8497b62
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-26 01:51:42 +0200

    realize win function

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,23 +14,36 @@ function getComputedChoice() {
   return choices[randomNumber];
 }
 
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = userChoice + " beats " + computerChoice + ". You win!";
+}
+
+function lose() {}
+
+function draw() {
+  console.log("DRAW");
+}
+
 function game(userChoice) {
   const computerChoice = getComputedChoice();
   switch (userChoice + computerChoice) {
     case "rs":
     case "pr":
     case "sp":
-      win();
+      win(userChoice, computerChoice);
       break;
     case "rp":
     case "ps":
     case "sr":
-      lose();
+      lose(userChoice, computerChoice);
       break;
     case "rr":
     case "pp":
     case "ss":
-      draw();
+      draw(userChoice, computerChoice);
       break;
   }
 }
