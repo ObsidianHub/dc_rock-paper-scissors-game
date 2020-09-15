07c160c2d6c60669c89eb8af94b80d459ccd171d
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-15 23:47:16 +0300

    create function for all combinations

--- rockPaperScissorsGame.js
+++ rockPaperScissorsGame.js
@@ -20,6 +20,27 @@ function convertToWord(letter) {
   return "Scissors";
 }
 
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
+
 function main() {
   rock_div.addEventListener("click", () => game("r"));
 
